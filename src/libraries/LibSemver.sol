9ad168f955407312d1ccec079e7d5b28d5749400
Tim Pechersky <tim.pechersky@gmail.com>
2024-09-18 21:31:31 +0300

    allow wider solidity version in libsemver

--- src/libraries/LibSemver.sol
+++ src/libraries/LibSemver.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity =0.8.20;
+pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/utils/Strings.sol";
 library LibSemver {
     error versionMissmatch(string message);
