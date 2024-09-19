33e54955e4b4444ba5e7b392698cdd7fd0a3b963
Tim Pechersky <tim.pechersky@gmail.com>
2024-09-19 22:48:12 +0300

    restore code index to fixed solidity pragma

--- src/ICodeIndex.sol
+++ src/ICodeIndex.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: CC0-1.0
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity 0.8.20;
 
 interface ICodeIndex {
     event Indexed(address indexed container, bytes32 indexed codeHash);
