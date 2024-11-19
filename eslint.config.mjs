20bf80f7e86cfe88c3915b8077f2745317a1ff32
Tim Pechersky <tim.pechersky@gmail.com>
2024-11-19 18:23:27 +0700

    remove dangle comma rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,23 +12,23 @@ const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
+  allConfig: js.configs.all
 });
 
 export default [
   {
-    ignores: ["deployments/", "coverage/**", "docs/templates/", "**/node_modules/"],
+    ignores: ["deployments/", "coverage/**", "docs/templates/", "**/node_modules/"]
   },
   ...compat.extends(
     "eslint:recommended",
     "plugin:promise/recommended",
     "plugin:prettier/recommended", // Add Prettier recommended config
-    "prettier",
+    "prettier"
   ),
   {
     plugins: {
       promise,
-      prettier, // Add Prettier plugin
+      prettier // Add Prettier plugin
     },
     languageOptions: {
       globals: {
@@ -38,14 +38,15 @@ export default [
         artifacts: "readonly",
         contract: "readonly",
         assert: "readonly",
-        web3: "readonly",
+        web3: "readonly"
       },
-      parser: tsParser,
+      parser: tsParser
     },
     rules: {
       "no-unused-vars": "warn",
       "prettier/prettier": "error", // Add Prettier as an ESLint rule
+      "comma-dangle": "off"
     },
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
-  },
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"]
+  }
 ];
