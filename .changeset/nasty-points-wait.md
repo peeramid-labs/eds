48570f031cd080d3e324b0244741452698a99bbd
Peersky <61459744+peersky@users.noreply.github.com>
2024-11-20 20:31:07 +0700

    Use another interface for more loose code index (#30)
    * added aux code indexer interface, fixed more typos
    
    * changed code indexer dependency

---
+++ .changeset/nasty-points-wait.md
@@ -0,0 +1,5 @@
+---
+"@peeramid-labs/eds": patch
+---
+
+added aux file for codeIndex interface in more loose pragma to be more reusable as library
--- src/abstracts/CodeIndexer.sol
+++ src/abstracts/CodeIndexer.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity >=0.8.0 <0.9.0;
 
-import "../ICodeIndex.sol";
+import "../ICodeIndexDep.sol";
 
 abstract contract CodeIndexer {
     //Create2 contract
